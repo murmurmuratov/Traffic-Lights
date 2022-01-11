c7d02954a3ec7d76a428f4e8ff137f5e2d89d46d
Alexander Muratov <MuratovAlexanderVasilievich@ya.ru>
2022-01-12 01:13:55 +0300

    Initial Commit

---
+++ Traffic Lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic Lights
+//
+//  Created by Александр Муратов on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
