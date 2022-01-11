a9b26e3140283b5d5712a774a9c8cc99b04507fa
Alexander Muratov <MuratovAlexanderVasilievich@ya.ru>
2022-01-12 02:59:11 +0300

    Squares became circles. The button became rounded. Colors became inactive (less saturated)

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var toggleButton: UIButton!
+
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        makeViewIntoCircle(view: redLightView)
+        makeViewIntoCircle(view: yellowLightView)
+        makeViewIntoCircle(view: greenLightView)
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        toggleButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
+    
+    func makeViewIntoCircle (view: UIView) {
+        view.layer.cornerRadius = view.layer.bounds.width / 2
+        view.clipsToBounds = true
+    }
 
+    @IBAction func makeButtonAction(_ sender: Any) {
 
+    }
+    
 }
 
